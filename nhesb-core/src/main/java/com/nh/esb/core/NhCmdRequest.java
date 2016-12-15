e37442dcd2af9b76749a0054071425c9e369212c
ninghao <jeffreyning@aliyun.com>
2016-12-15 15:05:05 +0800

    add request column

--- nhesb-core/src/main/java/com/nh/esb/core/NhCmdRequest.java
+++ nhesb-core/src/main/java/com/nh/esb/core/NhCmdRequest.java
@@ -45,7 +45,49 @@ public void setRemark(String remark) {
 	this.remark = remark;
 }
 
+private String subName;
+private String ext1;
+private String ext2;
+private String ext3;
+private String ext4;
+private String ext5;
 
+public String getSubName() {
+	return subName;
+}
+public void setSubName(String subName) {
+	this.subName = subName;
+}
+public String getExt1() {
+	return ext1;
+}
+public void setExt1(String ext1) {
+	this.ext1 = ext1;
+}
+public String getExt2() {
+	return ext2;
+}
+public void setExt2(String ext2) {
+	this.ext2 = ext2;
+}
+public String getExt3() {
+	return ext3;
+}
+public void setExt3(String ext3) {
+	this.ext3 = ext3;
+}
+public String getExt4() {
+	return ext4;
+}
+public void setExt4(String ext4) {
+	this.ext4 = ext4;
+}
+public String getExt5() {
+	return ext5;
+}
+public void setExt5(String ext5) {
+	this.ext5 = ext5;
+}
 public String getCallType() {
 	return callType;
 }
--- nhesb-core/src/main/java/com/nh/esb/core/NhCmdResult.java
+++ nhesb-core/src/main/java/com/nh/esb/core/NhCmdResult.java
@@ -14,6 +14,43 @@ private String resultData;
 private String errMsg="";
 private String requestId;
 private String remark;
+
+private String ext1;
+private String ext2;
+private String ext3;
+private String ext4;
+public String getExt1() {
+	return ext1;
+}
+public void setExt1(String ext1) {
+	this.ext1 = ext1;
+}
+public String getExt2() {
+	return ext2;
+}
+public void setExt2(String ext2) {
+	this.ext2 = ext2;
+}
+public String getExt3() {
+	return ext3;
+}
+public void setExt3(String ext3) {
+	this.ext3 = ext3;
+}
+public String getExt4() {
+	return ext4;
+}
+public void setExt4(String ext4) {
+	this.ext4 = ext4;
+}
+public String getExt5() {
+	return ext5;
+}
+public void setExt5(String ext5) {
+	this.ext5 = ext5;
+}
+private String ext5;
+
 public String getRemark() {
 	return remark;
 }
